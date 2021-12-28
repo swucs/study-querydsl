4cea3378d6d11efda64fce29e31147e3bf0ddafd
Hanna Park <swucs7@gmail.com>
2021-12-28 14:39:37 +0900

    다양한 조인 : inner join, theta join, outer join, 연관관게 없는 외부조인 등

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -2,9 +2,11 @@ package com.study.querydsl;
 
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.entity.Member;
 import com.study.querydsl.entity.QMember;
+import com.study.querydsl.entity.QTeam;
 import com.study.querydsl.entity.Team;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import static com.study.querydsl.entity.QMember.member;
+import static com.study.querydsl.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -138,5 +141,126 @@ public class QuerydslBasicTest {
         assertThat(memberQueryResults.getResults().size()).isEqualTo(2);
     }
 
+
+
+    @Test
+    public void aggregation() {
+        List<Tuple> members = queryFactory
+                .select(
+                        member.count()
+                        , member.age.sum()
+                        , member.age.avg()
+                        , member.age.max()
+                        , member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = members.get(0);
+
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+
+    }
+
+    @Test
+    public void groupBy() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
+    }
+
+    @Test
+    public void join() {
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("Member1", "Member2");
+    }
+
+    /**
+     * 연관관계가 없는 조인
+     * cross join
+     * 카테시안 곱이라고 했었음.
+     * 이 세타 조인은 외부조인은 되지 않음
+     */
+    @Test
+    public void thetaJoin() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+
+
+    }
+
+    /**
+     * 회원과 팀을 조회하면서 팀이름이 teamA인 팀만 조회, 멤버는 모두 조회
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> tuples = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : tuples) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 외부 조인
+     * 회원의 이름과 팀의 이름이 같은 대상 외부조인
+     */
+    @Test
+    public void join_on_no_relation() {
+
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamA"));
+
+        List<Tuple> tuples = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : tuples) {
+            System.out.println("tuple = " + tuple);
+        }
+
+    }
 
 }
