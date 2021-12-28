de985b08176252bcdf4e6e5fa54b7387723b66d6
Hanna Park <swucs7@gmail.com>
2021-12-28 13:39:01 +0900

    querydsl 검색 정렬 페이징 테스트

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package com.study.querydsl;
 
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.entity.Member;
 import com.study.querydsl.entity.QMember;
@@ -13,6 +14,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import java.util.List;
+
+import static com.study.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -61,16 +65,77 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl() {
-        QMember m = new QMember("m");
-
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("Member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("Member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("Member1");
 
+    }
+
+    @Test
+    public void search() {
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("Member1")
+                        , member.age.eq(10)
+                )
+                .fetch();
+
+        assertThat(members.size()).isEqualTo(1);
+    }
+
+
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("Member5", 100));
+        em.persist(new Member("Member6", 100));
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = members.get(0);
+        Member member6 = members.get(1);
+        Member memberNull = members.get(2);
+        assertThat(member5.getUsername()).isEqualTo("Member5");
+        assertThat(member6.getUsername()).isEqualTo("Member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+
+    @Test
+    public void paging1() {
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(members.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+
+        QueryResults<Member> memberQueryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(memberQueryResults.getTotal()).isEqualTo(4);
+        assertThat(memberQueryResults.getLimit()).isEqualTo(2);
+        assertThat(memberQueryResults.getOffset()).isEqualTo(1);
+        assertThat(memberQueryResults.getResults().size()).isEqualTo(2);
     }
 
 
