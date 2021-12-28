cdb2289fc34c8db9659b9a826b3d91e6f17c9bb6
Hanna Park <swucs7@gmail.com>
2021-12-28 12:34:52 +0900

    querydsl로 기본 조회

---
+++ src/main/java/com/study/querydsl/config/P6spyLogMessageFormatConfiguration.java
@@ -0,0 +1,17 @@
+package com.study.querydsl.config;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * query multi line custom configure
+ */
+@Configuration
+public class P6spyLogMessageFormatConfiguration {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(CustomP6spyFormatSql.class.getName());
+    }
+}
